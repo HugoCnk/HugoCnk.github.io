854e22f5129d0069c4a2145211530d85a5fcf9eb
HugoCnk <czerniakhugo@gmail.com>
2025-09-18 16:50:38 +0200

    website_v1.0

---
+++ script.js
@@ -0,0 +1,10 @@
+// Petit effet pour le formulaire
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  alert("Merci ! Votre message a bien été envoyé.");
+});
+// Petit effet pour le formulaire
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  alert("Merci ! Votre message a bien été envoyé.");
+});
